53968dc2174f7645e8e25813416ccfaa0da9ade1
Баглий Антон Павлович <bagliy_ap@studzone.local>
2019-01-30 00:29:35 +0300

    ADD: run and stop scripts with docker commands to run graylog

--- scripts/docker/rm.sh
+++ scripts/docker/rm.sh
@@ -2,5 +2,8 @@
 
 # example :
 #
-#docker rm -f $app 1>&2
-#echo $?
+
+docker rm -f mongo 1>&2
+docker rm -f elasticsearch 1>&2
+docker rm -f $app 1>&2
+echo $?
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -11,3 +11,20 @@
 #
 #docker run -d --name=$app --restart always $options $image 1>&2
 #echo $?
+
+options="-p $port:9000 -p 12201:12201 -p 514:514 -v $data_path/data:/data"
+
+iptables -t filter -N DOCKER
+
+docker run --name mongo -d --restart always mongo:3 1>&2
+docker run --name elasticsearch \
+    -e "http.host=0.0.0.0" -e "xpack.security.enabled=false" \
+    -d --restart always docker.elastic.co/elasticsearch/elasticsearch-oss:6.5.4 1>&2
+docker run --link mongo --link elasticsearch \
+    --name=$app --restart always \
+    $options \
+    -e GRAYLOG_WEB_ENDPOINT_URI="http://127.0.0.1:9000/api" \
+    -d graylog/graylog:3.0 1>&2
+
+CR=$?
+echo $CR
